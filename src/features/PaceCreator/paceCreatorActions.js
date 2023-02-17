250ebe778bcc0c647574fb95dca985bafeb556f6
CereaaalDev <97185101+CereaaalDev@users.noreply.github.com>
2023-02-17 17:04:44 +0100

    Rekursion mit dispatch

--- src/features/PaceCreator/paceCreatorActions.js
+++ src/features/PaceCreator/paceCreatorActions.js
@@ -3,9 +3,15 @@ import { contentAPI } from "../../api/content";
 
 export const getPlaylists = createAsyncThunk(
   "paceCreator/getPlaylists",
-  async (_, {rejectWithValue }) => {
+  async (url, {dispatch, rejectWithValue }) => {
     try {
-      const responsePlaylist = await contentAPI.get(`/me/playlists`);
+      let responsePlaylist = '';
+
+      if(!url){
+        responsePlaylist = await contentAPI.get(`/me/playlists?limit=50`);
+      }else{
+        responsePlaylist = await contentAPI.get(url);
+      }      
     //   const responsePlaylist = await contentAPI.get(`https://api.spotify.com/v1/me/tracks?offset=20&limit=20&locale=de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7,fr;q=0.6,la;q=0.5,es;q=0.4`);
    
     // const responsePlaylist = await contentAPI.get(`https://api.spotify.com/v1/playlists/37i9dQZF1EUMDoJuT8yJsl`);
@@ -14,6 +20,13 @@ export const getPlaylists = createAsyncThunk(
     //   if (responseTracks && responseTracks.data && responseArtists && responseArtists.data) {
     //     return {tracks: responseTracks.data, artists: responseArtists.data}
     //   }
+      if(responsePlaylist.data.next != null){
+        dispatch(getPlaylists(responsePlaylist.data.next))
+      }
+
+      return responsePlaylist.data.items;
+
+
     } catch (error) {
       console.log(error);
       return rejectWithValue(error.data);
